def28a3dcc33c170c67917746ef818308e85257b
Simo Antikainen <simo.antikainen@aalto.fi>
2018-06-24 21:50:16 +0300

    testing cause

--- models/note.js
+++ models/note.js
@@ -5,7 +5,7 @@ if ( process.env.NODE_ENV !== 'production' ) {
   console.log("This should not happen in production")
 }
 
-const url = "mongodb://simoa:spagettikoodari1337@ds161520.mlab.com:61520/hymooc_simo_tutorial_tuotanto"
+const url = "mongodb://simoa:spagettikoodari1337@ds161520.mlab.com:61520/hymooc_simo_tutorial"
 
 console.log("URI is", url)
 
