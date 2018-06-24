bc86ccee1d05b2a1587ba1a20dad6ecf539a908e
Simo Antikainen <simo.antikainen@aalto.fi>
2018-06-24 21:31:42 +0300

    fix

--- models/note.js
+++ models/note.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose')
 //environment variable
 if ( process.env.NODE_ENV !== 'production' ) {
   require('dotenv').config()
+  console.log("This should not happen in production")
 }
 
 const url = process.env.MONGODB_URI
+
+console.log("URI is", url)
 
 mongoose.connect(url)
 
